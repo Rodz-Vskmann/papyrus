6aedab2bcfba1a21bda3848ffc71d0cb555fabcd
Rodz-Vskmann <96752292+Rodz-Vskmann@users.noreply.github.com>
2023-08-13 12:54:13 -0300

    Delete Papyrus directory

--- Papyrus/account/scriptaccount.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('h1').addEventListener('click', function() {
-    gsap.to('h1', {
-        scale: 1.2,
-        duration: 0.5,
-        yoyo: true,
-        repeat: 1
-    });
-});
